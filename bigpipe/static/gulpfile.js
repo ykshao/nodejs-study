d5bf71a6dd1654b3fda195d77711586505acc96a
shaoyongkai <shaoyongkai@51talk.com>
2018-12-11 23:56:14 +0800

    删除文件

--- bigpipe/static/gulpfile.js
+++
@@ -1,6 +0,0 @@
-/*
- * 51Talk 自动化工程管理
- */
-
-var requireDir = require('require-dir');
-requireDir('./tasks');
--- bigpipe/static/tasks/components/components-images.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp-param')(require('gulp'), process.argv);
-
-gulp.task('components-images', function (project, type) {
-  return gulp.src([
-    'components/' + type + '/' + project + '/images/' + '/**/*.{jpg,png,gif,swf}',
-  ])
-    .pipe(gulp.dest('../components/' + type + '/' + project + '/images/'))
-});
-
