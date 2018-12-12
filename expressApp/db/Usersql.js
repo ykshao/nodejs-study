98464e95ef6d0e743f97b9dcd4384d8c6d818a26
252_100006306464_768662717 <252_100006306464_768662717@git.cloud.tencent.com>
2018-12-12 13:26:37 +0800

    项目分离

--- expressApp/db/Usersql.js
+++
@@ -1,6 +0,0 @@
-var sql = {
-    insert: 'INSERT INTO test(userId,password,email) VALUES(?,?,?)',
-    queryAll: 'SELECT * FROM test',
-    getUserById: 'SELECT * FROM test WHERE userId = ? '
-};
-module.exports = sql;
--- expressApp/util/filter.js
+++
@@ -1,7 +0,0 @@
-exports.authorize = function (req, res, next) {
-    if (!req.session.userName) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
-};
